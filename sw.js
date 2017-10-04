046e34d52ed05d3fa70e74002e22220dba49cd0a
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-10-04 12:37:56 +0700

    new gh

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 (global => {
   'use strict';
-var now = new Date();
+
+  var now = new Date();
   var onejan = new Date(now.getFullYear(), 0, 1);
   var week = Math.ceil( (((now - onejan) / 86400000) + onejan.getDay() + 1) / 7 );
   var ver = now.toISOString().substring(0, 7);
   var versionCache = '-chuckNorris-' + ver + '-' + week
-
 
   
   // Load the sw-tookbox library.
